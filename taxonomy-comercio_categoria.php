724849709c7731746d3518ef33c34ee584da7d5a
crlstve <ecarles10@gmail.com>
2024-11-11 21:00:36 +0100

    quitar form de categoria

--- taxonomy-comercio_categoria.php
+++ taxonomy-comercio_categoria.php
@@ -1,9 +1,8 @@
-<?php
-get_header(); // Llama al encabezado del tema
-?>
+<?php get_header(); ?>
+
     <main class="contain 2xl:max-w-7xl mx-auto px-6">
 
-    <?= get_template_part('partials/filtros'); ?> 
+        <?= get_template_part('partials/filtros'); ?> 
 
         <?= get_template_part('partials/grid-comercios'); ?>
 
