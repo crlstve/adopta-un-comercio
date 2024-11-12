f2318ac4cf52277675bcab635eb3a63c112f96a0
crlstve <ecarles10@gmail.com>
2024-11-12 20:48:23 +0100

    Número de adotpados

--- header.php
+++ header.php
@@ -17,9 +17,13 @@
 <body <?php body_class(''); ?>>
 	<?php wp_body_open(); ?>
 	<header id="header" class="contain 2xl:max-w-7xl mx-auto px-6">
-		<div class="mx-auto py-3 flex flex-row justify-between">
-			<div id="logo" class="flex flex-row w-1/2 md:w-2/6 mx-auto">
+		<div class="mx-auto py-3 flex flex-row justify-between gap-4">
+			<div id="logo" class="relative flex flex-col w-1/2 md:w-2/6 mx-auto gap-6">
 				<?= (has_custom_logo()) ? the_custom_logo() : ''; ?>
+				<span class="bg-orange text-dark md:text-lg font-bold w-fit self-center px-6 py-3 text-center">
+				<?php $categoria_slug = ['adopta', 'adoptado_void']; $args = [ 'post_type'      => 'comercios', 'posts_per_page' => -1, 'tax_query'      => [ [ 'taxonomy' => 'comercio_categoria', 'field'    => 'slug', 'terms'    => array('adoptado','adoptado_void'), 'operator' => 'IN', ], ], 'fields' => 'ids', ]; $query = new WP_Query($args); $total_entradas = $query->found_posts; ?>
+				<?= esc_html('¡Ya son ' . $total_entradas.' comercios adoptados!'); ?>
+			</span>
 			</div>
 		</div>
 	</header>
