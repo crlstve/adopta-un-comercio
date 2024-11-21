3807400416f8a4a490b80ed0b32b8c038f4de30e
crlstve <116392303+crlstve@users.noreply.github.com>
2024-11-21 20:18:31 +0100

    Updates (#2)
    * test
    
    * test
    
    * string translate on footer

--- footer.php
+++ footer.php
@@ -19,17 +19,21 @@
             </div>
             <div class="contain 2xl:max-w-5xl mb-8 flex flex-row flex-wrap justify-center gap-12 mx-auto my-8">
                 <div class="px-6 md:px-0">
-                    <p class="text-xs text-white text-center">SIBERIA, promotora de la plataforma ADOPTA UN COMERCIO, única y exclusivamente pone en contacto a los posibles afectados con los diferentes influencers que están tratando de dar visibilidad a los afectados y/o ayudarlos, por tanto cualquier ayuda que se reciba se deberá ingresar o entregar directamente a los afectados.</p>
-                    <p class="text-xs text-white text-center">En el supuesto de que el beneficiario reciba una ayuda económica de un tercero (ya sea una entidad pública, privada o particular) destinada a mitigar o compensar los daños sufridos por la DANA, el beneficiario asume de forma expresa la obligación de cumplir con las obligaciones tributarias que correspondan. Esto incluye, pero no se limita, al cumplimiento de los deberes fiscales en materia de impuestos sobre la renta, patrimonio u otros tributos aplicables, conforme a la legislación vigente. El beneficiario declara estar informado de que dicha ayuda económica puede constituir una ganancia patrimonial o renta imponible sujeta a tributación y que será responsable de realizar las declaraciones y pagos que correspondan ante las autoridades fiscales competentes.</p>
+                    <p class="text-xs text-white text-center"><?php esc_html_e('SIBERIA, promotora de la plataforma ADOPTA UN COMERCIO, única y exclusivamente pone en contacto a los posibles afectados con los diferentes influencers que están tratando de dar visibilidad a los afectados y/o ayudarlos, por tanto cualquier ayuda que se reciba se deberá ingresar o entregar directamente a los afectados.','adopta');?>
+                    </p>
+                    <p class="text-xs text-white text-center">
+                    <?php esc_html_e('En el supuesto de que el beneficiario reciba una ayuda económica de un tercero (ya sea una entidad pública, privada o particular) destinada a mitigar o compensar los daños sufridos por la DANA, el beneficiario asume de forma expresa la obligación de cumplir con las obligaciones tributarias que correspondan. Esto incluye, pero no se limita, al cumplimiento de los deberes fiscales en materia de impuestos sobre la renta, patrimonio u otros tributos aplicables, conforme a la legislación vigente. El beneficiario declara estar informado de que dicha ayuda económica puede constituir una ganancia patrimonial o renta imponible sujeta a tributación y que será responsable de realizar las declaraciones y pagos que correspondan ante las autoridades fiscales competentes.','adopta'); ?>
+                    
+                  </p>
                 </div>
             </div>
             <hr class="contain 2xl:max-w-7xl mx-auto my-8">
             <nav class="contain 2xl:max-w-7xl mx-auto my-8 flex flex-row flex-wrap justify-center gap-3 text-white">
-                <a href="<?= esc_url('https://siberia.es/aviso-legal/')?>" target="_blank" class="text-sm self-center">AVISO LEGAL</a>
+                <a href="<?= esc_url('https://siberia.es/aviso-legal/')?>" target="_blank" class="text-sm self-center"><?php esc_html_e('AVISO LEGAL','adopta'); ?></a>
                 <span class="text-sm text-white">|</span>
-                <a href="<?= esc_url('https://siberia.es/politica-de-privacidad/')?>" target="_blank" class="text-sm self-center">POLÍTICA DE PRIVACIDAD</a>
+                <a href="<?= esc_url('https://siberia.es/politica-de-privacidad/')?>" target="_blank" class="text-sm self-center"><?php esc_html_e('POLÍTICA DE PRIVACIDAD','adopta'); ?></a>
                 <span class="text-sm text-white">|</span>
-                <a href="<?= esc_url('https://siberia.es/politica-de-cookies/')?>" target="_blank" class="text-sm self-center">POLÍTICA DE COOKIES</a>
+                <a href="<?= esc_url('https://siberia.es/politica-de-cookies/')?>" target="_blank" class="text-sm self-center"><?php esc_html_e('POLÍTICA DE COOKIES','adopta'); ?></a>
             </nav>
             <script>
                     function copyToClipboard(text) {
