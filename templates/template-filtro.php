d495281e268f20df3529b3f7693001dfa645aee9
crlstve <ecarles10@gmail.com>
2024-12-01 11:51:28 +0100

    New Templates

--- templates/template-filtro.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-    <main class="contain 2xl:max-w-7xl mx-auto px-6 my-12">
-
-        <?= get_template_part('partials/grid-comercios-filtros'); ?>
-
-    </main>
-<?php get_footer(); ?>
